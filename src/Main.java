5dfa87b67539d41120a4964022405efb9a99b781
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2024-12-07 17:17:53 -0300

    refactor Main

--- src/Main.java
+++ src/Main.java
@@ -6,64 +6,81 @@ import lexer.tokens.Token;
 import parser.Parser;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Lexer lexer = new Lexer();
-        List<String> files = List.of(
-                "src/resources/lsi/file-without-lexical-errors.lsi",
-                "src/resources/lsi/file-with-lexical-errors.lsi"
-        );
-        Scanner scanner = new Scanner(System.in);
+    public static List<String> files = List.of(
+            "src/resources/lsi/fibonacci.lsi",
+            "src/resources/lsi/fibonacci-with-syntax-errors.lsi",
+            "src/resources/lsi/recursive-factorial.lsi",
+            "src/resources/lsi/recursive-factorial-with-syntax-errors.lsi"
+    );
 
-        printWelcomeMessage();
-        promptOptions();
+    public static final Lexer lexer = new Lexer();
+    public static final Parser parser = new Parser();
 
-        int option = scanner.nextInt();
+    public static void main(String[] args) {
+        Logger.prompt("Lexer & LL(1) Parser Implementation");
+        Logger.prompt("-----------------------------------");
+
+        int option = getInputOption();
         while (option != 0) {
-            if (option < 0 || option > 2) {
-                System.out.println("Option unavailable");
-                promptOptions();
-                option = scanner.nextInt();
+            if (option < 0 || option > files.size()) {
+                Logger.error(String.format("Invalid input. Please, enter an integer number between %s and %s", 0, files.size()));
+                option = getInputOption();
                 continue;
             }
 
-            Map.Entry<List<Token>, Map<String, Token>> lexicalResult = lexer.scan(files.get(option - 1));
-            List<Token> tokens = lexicalResult.getKey();
-            Map<String, Token> symbolsTable = lexicalResult.getValue();
-            printLexicalResult(tokens, symbolsTable);
-            new Parser().parse(tokens, symbolsTable);
+            String filename = files.get(option - 1);
+            try {
+                runCompiler(filename);
+            } catch (IOException e) {
+                Logger.error(String.format("Unable to read %s file at the moment. Please, try again later.", filename));
+            }
 
-            promptOptions();
-            option = scanner.nextInt();
+            option = getInputOption();
         }
-
-        System.out.println("Exiting...");
     }
 
-    private static void printWelcomeMessage() {
-        System.out.println("Hand-written Lexer");
-        System.out.println("---------------------------");
-        System.out.println("This lexer is able to recognize relational operators, function and variable identifiers and integer numbers greater than zero");
-        System.out.println("---------------------------");
-    }
-
-    private static void promptOptions() {
-        System.out.println("Two source files are available to be scanned:");
-        System.out.println("(1) - /src/resources/lsi/file-without-lexical-errors.resources.lsi (file without lexical errors)");
-        System.out.println("(2) - /src/resources/lsi/file-with-lexical-errors.resources.lsi (file with lexical errors)");
-        System.out.println("(0) - Enter 0 to exit");
+    private static void runCompiler(String sourceFilename) throws IOException {
+        Map.Entry<List<Token>, Map<String, Token>> lexicalResult = lexer.scan(sourceFilename);
+        List<Token> tokens = lexicalResult.getKey();
+        Map<String, Token> symbolsTable = lexicalResult.getValue();
+        printLexicalResult(tokens, symbolsTable);
+        parser.parse(tokens, symbolsTable);
     }
 
     private static void printLexicalResult(List<Token> tokens, Map<String, Token> symbolsTable) {
-        System.out.println("----------TOKENS-----------");
+        Logger.prompt("TOKENS");
         System.out.println(tokens);
         System.out.println();
-        System.out.println("-------SYMBOLS TABLE-------");
+        Logger.prompt("SYMBOLS");
         System.out.println(symbolsTable);
         System.out.println();
+    }
+
+    private static int getInputOption() {
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        promptOptions();
+
+        try {
+            option = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            option = -1;
+        }
+
+        return option;
+    }
+
+    private static void promptOptions() {
+        Logger.prompt(String.format("%s LSI source files are available:", files.size()));
+        files.forEach((file) -> {
+            Logger.prompt(String.format("(%s) - %s", files.indexOf(file) + 1, file));
+        });
+        Logger.prompt("(0) - Enter 0 to exit");
     }
 }
